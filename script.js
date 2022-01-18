fdf064da6a5244f12744a83050b8de442cea6ab5
GitHub7402 <77265634+GitHub7402@users.noreply.github.com>
2022-01-18 09:48:36 +0900

    Add Reset CSS

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+const copyLinkBtn = document.querySelector("#copyLinkBtn");
 const copyBtn = document.querySelector("#copyBtn");
 const copyModal = document.querySelector("#copyModal");
 
 const SCRIPT_SRC = `<script src="https://kit.fontawesome.com/6478f529f2.js" crossorigin="anonymous"></script>`;
+const RESET_CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css";
 
 function hideModal() {
     copyModal.style.display = "none";
 }
 
+function copyLink() {
+    navigator.clipboard.writeText(RESET_CSS_URL);
+}
+
+if(copyLinkBtn) {
+    copyLinkBtn.addEventListener("click", copyLink);
+}
 function copyCode() {
     navigator.clipboard.writeText(SCRIPT_SRC);
     copyModal.style.display = "flex";
